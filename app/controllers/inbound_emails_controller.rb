8d66b30dfc06f5d2f5ccde7fb4dd0411d594ff79
Alec Turnbull <alec@threering.com>
2012-11-10 12:59:32 -0500

    etc'

--- app/controllers/inbound_emails_controller.rb
+++ app/controllers/inbound_emails_controller.rb
@@ -3,23 +3,8 @@ class InboundEmailsController < ApplicationController
 
   def receive
 
-    @recipient = params["to"]
     @sender = params["from"]
     @headers = params["headers"]
-
-    # @pledge = Pledge.find(@pledge_id)
-
-    if @pledge.recipient = nil
-      @pledge.recipient = @recipient
-      @pledge.sender = @sender
-      @pledge.save
-      # first email being sent
-      # save data to pledge
-    else 
-      # response
-      # check time
-      # donate
-    end
 
     DonorMailer.test(@sender, @headers).deliver
 
