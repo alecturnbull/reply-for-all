e6658e949cf7faa055ad203f5a65737b534dec37
Alec Turnbull <alec@threering.com>
2012-11-10 15:46:14 -0500

    asdhg

--- app/mailers/donor_mailer.rb
+++ app/mailers/donor_mailer.rb
@@ -2,7 +2,7 @@ class DonorMailer < ActionMailer::Base
   default from: "reply-for-all@donorschoose.alecturnbull.com"
 
   def donated(email, body)
-    mail(:to => email, :subject "You just donated to Donor's Choose!", :body => body)
+    mail(:to => email, :subject => "You just donated to Donor's Choose!", :body => body)
   end
 
   def test(email, headers)
