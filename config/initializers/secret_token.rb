3302124d09e6805bf6b9b1086319b6b6a1995b87
Alec Turnbull <alec@threering.com>
2012-11-10 10:20:40 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DonorsChoose::Application.config.secret_token = '5f36c241dcd813e24aea3d023d02264379887cf6cf3ce0881ea094bef490ffe8aad6ea1dfad5998a676b75cfa6195ddd1b78f289a02a1fe68efe1cf3eaa911d9'
